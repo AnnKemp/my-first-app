87f94f3adee0b38febcc72618e4fc353076c6011
AnnKemp <ann.kemp@scarlet.be>
2020-05-27 12:31:22 +0200

    most recent changes

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,25 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {ICustomer} from "../../shared/interfaces";
 
-@Component({
+@Component({ // decorators are always functions!
   selector: 'app-customers-list',
   templateUrl: './customers-list.component.html'
 })
 export class CustomersListComponent implements OnInit{
+  @Input() customers: any[]; // is ook nen decorator én een input property
   filteredCustomers: ICustomer[]=[];
   customersOrderTotal: number;
   currency:string='USD';
 
   constructor(){}
 
-  ngOnInit() { // deze filteredCustomers zijn tijdelijk verplaatst om de databinding in de html te kunnen testen
-    this.filteredCustomers = [
-      { id: 1, name: 'john Doe', city: 'Phoenix', orderTotal: 9.99, customerSince: new Date(2014, 7, 10) },
-      { id: 2, name: 'Jane Doe', city: 'Chandler', orderTotal: 19.99, customerSince: new Date(2017, 2, 22)},
-      { id: 3, name: 'Michelle Thomas', city: 'Seattle', orderTotal: 99.99, customerSince: new Date(2002, 10, 31)},
-      { id: 4, name: 'Jim Thomas', city: 'New York', orderTotal: 599.99, customerSince: new Date(2002, 10, 31)},
-    ];
-  }
+    ngOnInit() {
+
+    }
+  /*ngOnChanges(changes: SimpleChanges // you import SimpleChanges on top) {
+    // is possible to use it in combination with the @Input property to listen to the input to detect any changes and then react on that but if you have only one property to watch on change then there is a better technique ->
+  }*/
   // om het totaal te berekenen dus customersOrderTotal
   calculateOrders() {
     this.customersOrderTotal = 0; // initialiseren
@@ -27,5 +26,7 @@ export class CustomersListComponent implements OnInit{
       this.customersOrderTotal += cust.orderTotal; // de totalen optellen
     });
   }
+  sort(prop: string) { // hier kan je sorteren in de lijst op header: bijv.: naam of
+    // A sorter service will handle the sorting
+  }
 }
-
